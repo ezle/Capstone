133d22c5e01da6eb1e4e67a2b7aac6bc801da810
E <le.elton1@gmail.com>
2024-07-31 18:58:25 -0700

    Intro

---
+++ tealeaf/src/test/java/com/capstone/tealeaf/TealeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.tealeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TealeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
